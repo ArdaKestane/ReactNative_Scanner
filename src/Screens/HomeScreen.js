87a7408d50dcf1cac386a397cfb6cae9e6cbb1d6
Arda Kestane <ardakestane@hotmail.com>
2023-07-08 15:56:06 +0300

    Fixed ComponentData not showing on HomeScreen

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,45 +1,12 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, Image, ActivityIndicator} from 'react-native';
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
 const HomeScreen = ({componentsData}) => {
-  const thirtyDaysAgo = new Date();
-  thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
-
-  const filteredReceipts = componentsData.filter(c => {
-    const receiptDateParts = c.date.split('-');
-    const receiptDate = new Date(
-      parseInt(receiptDateParts[0]),
-      parseInt(receiptDateParts[1]) - 1,
-      parseInt(receiptDateParts[2]),
-    );
-    return receiptDate > thirtyDaysAgo;
-  });
-
-  const totalReceipts = filteredReceipts.length;
-  const totalAmount = filteredReceipts.reduce(
-    (total, c) => total + parseFloat(c.amount),
+  const totalReceipts = componentsData.length;
+  const totalAmount = componentsData.reduce(
+    (total, c) => total + parseFloat(c.amount || 0),
     0,
   );
-
-  const [loadingAmount, setLoadingAmount] = useState(true);
-  const [loadingReceipts, setLoadingReceipts] = useState(true);
-
-  useEffect(() => {
-    const delay = setTimeout(() => {
-      setLoadingAmount(false);
-    }, 500); // Simulate a 2-second delay
-
-    return () => clearTimeout(delay);
-  }, []);
-
-  useEffect(() => {
-    // Simulate loading delay with a timeout
-    const delay = setTimeout(() => {
-      setLoadingReceipts(false);
-    }, 500); // Simulate a 2-second delay
-
-    return () => clearTimeout(delay);
-  }, []);
 
   return (
     <View style={styles.container}>
@@ -50,28 +17,12 @@ const HomeScreen = ({componentsData}) => {
       />
 
       <View style={styles.amountContainer}>
-        {loadingAmount ? (
-          <ActivityIndicator
-            size="small"
-            color="black"
-            style={styles.loadingIndicator}
-          />
-        ) : (
-          <Text style={styles.amountValue}>₺{totalAmount.toFixed(2)}</Text>
-        )}
+        <Text style={styles.amountValue}>₺{totalAmount.toFixed(2)}</Text>
         <Text style={styles.amountText}>spent</Text>
       </View>
       <View style={styles.infoContainer}>
         <Text style={styles.infoText}>With a total of</Text>
-        {loadingReceipts ? (
-          <ActivityIndicator
-            size="small"
-            color="black"
-            style={styles.loadingIndicator}
-          />
-        ) : (
-          <Text style={styles.infoValue}>{totalReceipts}</Text>
-        )}
+        <Text style={styles.infoValue}>{totalReceipts}</Text>
         <Text style={styles.infoText}>receipts</Text>
       </View>
     </View>
@@ -128,9 +79,6 @@ const styles = StyleSheet.create({
   image: {
     width: 200,
     height: 200,
-  },
-  loadingIndicator: {
-    marginLeft: 10,
   },
 });
 
